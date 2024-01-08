8642dc8e10b1dcced10d07fb43245d531486cd64
Mateusz Kuzmik <mateusz.kuzmik@allegro.pl>
2024-01-08 12:42:38 +0100

    scripts finished

--- demo.sql
+++ demo.sql
@@ -1,6 +1,93 @@
------- UTILS ---------------------------------------------------------------
+--- PAGE ---
+
+-- Define table
+DROP TABLE IF EXISTS person;
+CREATE TABLE person
+(
+    id   serial primary key,
+    name varchar(255) NOT NULL
+);
+
+
+-- Fill it with data
+INSERT INTO person(name)
+VALUES ('Michael Scott'),
+       ('Dwight Schrute'),
+       ('Stanley Hudson');
+
+
+-- Lookup the data
+SELECT *
+FROM person;
+
+
+-- Lookup the data with tuple id
+SELECT ctid, *
+FROM person;
+-- ctid (page_number, offset)
+
+
+-- Update the first row
+UPDATE person
+SET name = 'Michael Klump'
+WHERE name = 'Michael Scott';
+
+
+-- Lookup the data with tuple id
+SELECT ctid, *
+FROM person;
+
+-- Case 1 - why tuple id hs changed? Why order has changed?
+
+
+-- Load pageinspect extension, which allows us to see details of the pages
 CREATE EXTENSION IF NOT EXISTS pageinspect;
 
+
+-- Display the content of page 0
+SELECT page
+FROM get_raw_page('person', 0) as page;
+-- copy it into text file
+-- Bonus material, how data is aligned into a page: https://www.interdb.jp/pg/pgsql01.html#_1.3.
+
+
+-- A proof that single page has 8kB, no matter how many rows it stores
+SELECT pg_column_size(page)
+FROM get_raw_page('person', 0) as page;
+
+
+-- More readable way to inspect the page - heap_page_items
+SELECT *
+FROM heap_page_items(get_raw_page('person', 0));
+
+
+--- MVCC ---
+
+SELECT lp, t_ctid, t_xmin, t_xmax, t_data
+FROM heap_page_items(get_raw_page('person', 0));
+
+
+-- current transaction id (txid)
+SELECT txid_current();
+
+
+-- remove tuples that are no longer needed
+VACUUM FULL;
+
+
+-- reading tuple flags
+SELECT heap_tuple_infomask_flags(t_infomask, t_infomask2)
+FROM heap_page_items(get_raw_page('person', 0));
+-- all tuples are frozen (t_xmin not comparable with newer txids)
+
+
+--- INDEX ---
+
+-- There is a default index on primary key (id)
+select *
+from bt_page_items('person_pkey', 1);
+
+-- utils
 CREATE OR REPLACE FUNCTION array_reverse(anyarray) RETURNS anyarray AS
 $$
 SELECT ARRAY(
@@ -28,132 +115,38 @@ SELECT hex_to_int(array_to_string(array_reverse(string_to_array(left($1, 11), ' 
 $$ LANGUAGE 'sql' STRICT
                   IMMUTABLE;
 
---------------------------------------------------------------------------------------------
-
-
--- START
-----------------------------------------------------------
-
-DROP TABLE IF EXISTS person;
-CREATE TABLE person
-(
-    id   serial primary key,
-    name varchar(255) NOT NULL
-);
-
-INSERT INTO person(name)
-VALUES ('Kapitan Bomba');
-INSERT INTO person(name)
-VALUES ('Chorąży Torpeda');
-INSERT INTO person(name)
-VALUES ('Sułtan Kosmitów');
-
-SELECT ctid, * FROM person;
--- ctid (page_no, offset)
-
--- ctid - tuple id (NR_STRONY, OFFSET)
-
-----------------------------------------------------------
-
-UPDATE person
-SET name = 'Matka Sultana Kosmitów'
-WHERE name = 'Sułtan Kosmitów';
-
-SELECT ctid, *
-FROM person WHERE ctid = (0,4);
-
-SELECT page FROM get_raw_page('person', 0) as page;
--- whyyyy: https://www.interdb.jp/pg/pgsql01.html#_1.3.
-
-SELECT pg_column_size(page) FROM get_raw_page('person', 0) as page;
-
------------------------------------------------------------
-
-SELECT '(0,' || lp || ')' as ctid,
-       t_ctid             as reference_to_ctid,
-       t_xmin,
-       t_xmax,
-       t_data
-FROM heap_page_items(get_raw_page('person', 0));
-
-SELECT * FROM heap_page_items(get_raw_page('person', 0));
-
--- mvcc - multi-version concurrency control
-
+-- More readable way
 select data_to_int(data) as key, ctid
 from bt_page_items('person_pkey', 1);
 
--- Pytanie: Dlaczego klucz 3 w indeksie nadal wskazuje na ctid (0,3)?
 
--- Odpowiedź: Bo HOT: https://www.interdb.jp/pg/pgsql07.html#_7.1.
-
--------------------------------------------------------------
-
-DELETE
-FROM person
-WHERE name = 'Chorąży Torpeda';
-
-SELECT '(0,' || lp || ')' as ctid,
-       t_ctid             as reference_to_ctid,
-       t_xmin,
-       t_xmax,
-       t_data
-FROM heap_page_items(get_raw_page('person', 0));
-
-select data_to_int(data) as key, ctid
-from bt_page_items('person_pkey', 1);
-
---------------------------------------------------------------
-
-VACUUM FULL;
-
-SELECT '(0,' || lp || ')' as ctid,
-       t_ctid             as reference_to_ctid,
-       t_xmin,
-       t_xmax,
-       t_data
-FROM heap_page_items(get_raw_page('person', 0));
-
-select data_to_int(data) as key, ctid
-from bt_page_items('person_pkey', 1);
-
----------------------------------------------------------------
-
+-- generate a lot of data for the index
 INSERT INTO person(name)
 SELECT md5(random()::text)
 FROM generate_series(1, 1000000);
-ANALYSE person;
 
-EXPLAIN
-SELECT *
-FROM person
-WHERE id = 50;
+-- confirm that data is there
+SELECT count(*)
+FROM person;
 
-EXPLAIN
-SELECT *
-FROM person
-WHERE id > 100000;
+-- Task - find id = 500000 in the btree
 
--- pytanie: dlaczego w drugim zapytaniu nie patrzymy na indeks?
-
----- mastering btree -----------------------------------------------
-
--- znajdźmy teraz klucz id=400'000 w indeksie btree
-
-SELECT *
+-- Read root page number from the metapage
+SELECT root
 FROM bt_metap('person_pkey');
+
 
 select data_to_int(data) as key,
        ctid,
        dead
 from bt_page_items('person_pkey', 412);
 
+
 select data_to_int(data) as key,
        ctid,
        dead
 from bt_page_items('person_pkey', 984);
--- pierwszy wiersz to odnośnik do następnej strony
--- drugi wiersz to odnośnik do poprzedniej strony
+
 
 select data_to_int(data) as key,
        ctid,
@@ -162,24 +155,36 @@ from bt_page_items('person_pkey', 1098);
 
 SELECT *
 FROM person
-WHERE ctid = '(3333,39)';
+WHERE ctid = '(3333,40)';
 
-----------------------------------------
+--- HOT ---
 
+-- noinspection SqlWithoutWhere
 UPDATE person
-SET name='kapitan bomba'
-WHERE id = 400000;
+SET name = 'Kelly Kapoor';
+-- benchmark
 
-SELECT *
-FROM person
-WHERE ctid = '(3333,39)';
+DROP TABLE IF EXISTS person_v2;
+CREATE TABLE person_v2
+(
+    id   serial primary key,
+    name varchar(255) NOT NULL
+) WITH (fillfactor=40);
+
+INSERT INTO person_v2(name)
+SELECT md5(random()::text)
+FROM generate_series(1, 1000000);
+
+-- noinspection SqlWithoutWhere
+UPDATE person_v2
+SET name = 'Kelly Kapoooor';
+-- benchmark with person (v1)
+
 
 select data_to_int(data) as key,
        ctid,
        dead
-from bt_page_items('person_pkey', 1098)
-WHERE data_to_int(data) = 400000;
+from bt_page_items('person_v2_pkey', 1098);
 
 SELECT *
-FROM person
-WHERE ctid = '(8333,43)';
+FROM heap_page_items(get_raw_page('person_v2', 8503));
